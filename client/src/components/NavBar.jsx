5190ff2d14ae4187d35298741aa78ef6ca8424f5
harishihan-c <harishihan@outlook.com>
2025-10-27 23:38:14 +0530

    feat: Add basic layout for nav bar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
-  )
-}
+    <div className="w-2xl  mx-auto mt-2 px-10 py-4 flex justify-between items-center rounded-full border ">
+      <a href="#home">Home</a>
+      <a href="#about">About</a>
+      <a href="#skills">Skills</a>
+      <a href="#projects">Projects</a>
+      <a href="#contact">Contact</a>
+    </div>
+  );
+};
 
-export default NavBar
+export default NavBar;
