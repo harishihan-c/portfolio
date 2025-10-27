5e62d3d51d2a8efedf1e14b796332df9fc73ee52
harishihan-c <harishihan@outlook.com>
2025-10-27 23:38:49 +0530

    feat: connect all nav bar elements to therir respective sections

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import NavBar from "./components/NavBar";
+import Home from "./sections/Home/Home";
+import About from "./sections/About/About";
+import Skills from "./sections/Skills/Skills";
+import Projects from "./sections/Projects/Projects";
+import Contact from "./sections/Contact/Contact";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <NavBar />
+      <section id="home" className="h-screen">
+        <Home />
+      </section>
+      <section id="about" className="h-screen">
+        <About />
+      </section>
+      <section id="skills" className="h-screen">
+        <Skills />
+      </section>
+      <section id="projects" className="h-screen">
+        <Projects />
+      </section>
+      <section id="contact" className="h-screen">
+        <Contact />
+      </section>
+    </div>
+  );
+};
 
-export default App
+export default App;
