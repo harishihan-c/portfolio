78a03acf186d6392da93641209c953548629a106
harishihan-c <harishihan@outlook.com>
2025-10-28 16:11:48 +0530

    feat: Add auto active selection on nav elements when scrolling

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,13 +1,39 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const NavBar = () => {
   const [activeSection, setActiveSection] = useState("home");
-  const activeColor = "bg-[#D7FF00] w-28 px-2 py-3 rounded-full"
+  const activeColor = "bg-[#D7FF00] ";
+
+  useEffect(() => {
+    const sectionArray = ["home", "about", "skills", "projects", "contact"];
+
+    const handleScroll = () => {
+      sectionArray.forEach((id) => {
+        const section = document.getElementById(id);
+
+        if (section) {
+          const top = section.offsetTop;
+          const height = section.offsetHeight;
+
+          if (
+            window.scrollY >= top - height / 2 &&
+            window.scrollY < top + height / 2
+          ) {
+            setActiveSection(id);
+          }
+        }
+      });
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   return (
-    <div className="w-2xl  mx-auto mt-5 px-10 py-2 flex justify-between items-center rounded-full border text-center ">
+    <div className="fixed left-1/2 -translate-x-1/2 w-2xl mt-5 px-10 py-2 flex justify-between items-center rounded-full border text-center ">
       <a
         onClick={() => setActiveSection("home")}
-        className={`${activeSection === "home" ? activeColor : ""} `}
+        className={`${
+          activeSection === "home" ? activeColor : ""
+        } w-28 px-2 py-3 rounded-full`}
         href="#home"
       >
         Home
@@ -15,27 +41,28 @@ const NavBar = () => {
       <a
         onClick={() => setActiveSection("about")}
         href="#about"
-        className={`${activeSection === "about" ? activeColor : ""}`}
+        className={`${activeSection === "about" ? activeColor : ""} w-28 px-2 py-3 rounded-full`}
       >
         About
       </a>
       <a
         onClick={() => setActiveSection("skills")}
-        className={`${activeSection === "skills" ? activeColor : ""}`}
+        className={`${activeSection === "skills" ? activeColor : ""} w-28 px-2 py-3 rounded-full`}
         href="#skills"
       >
         Skills
       </a>
       <a
         onClick={() => setActiveSection("projects")}
-        className={`${activeSection === "projects" ? activeColor : ""}  `}
+        className={`${activeSection === "projects" ? activeColor : ""} w-28 px-2 py-3 rounded-full  `}
         href="#projects"
       >
         Projects
       </a>
+      
       <a
         onClick={() => setActiveSection("contact")}
-        className={`${activeSection === "contact" ? activeColor : ""}`}
+        className={`${activeSection === "contact" ? activeColor : ""} w-28 px-2 py-3 rounded-full`}
         href="#contact"
       >
         Contact
