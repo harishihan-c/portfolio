3490872849cf211c4bfdf15f41d195597b6a456b
harishihan-c <harishihan@outlook.com>
2025-10-28 20:24:42 +0530

    feat: add home all texts and styles

--- client/src/sections/Home/Home.jsx
+++ client/src/sections/Home/Home.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div className='h-screen'>Home</div>
-  )
-}
+    <div className=" flex flex-col justify-center ml-14">
+      <p className="font-integral-extra-bold text-6xl sm:text-7xl mt-6 ">
+        Hello,
+      </p>
 
-export default Home
+      <p className="font-integral-extra-bold text-6xl sm:text-7xl mt-6">
+        I'm{" "}
+        <span className=" bg-black text-[#D7FF00] px-5 pb-4 pt-0 inline-block ">
+
+          Harry,
+        </span>
+      </p>
+
+      <div className="font-integral-extra-bold text-6xl sm:text-7xl mt-6 text-center"><p >A FullStack </p>
+      <p>Developer, Designer</p> <p> and an Artist</p></div>
+    </div>
+  );
+};
+
+export default Home;
