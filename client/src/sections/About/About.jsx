02a51212440de6eb3a71d2689f93ae11fcffae8f
harishihan-c <harishihan@outlook.com>
2025-11-01 22:15:08 +0530

    feat: add extra div and hide that

--- client/src/sections/About/About.jsx
+++ client/src/sections/About/About.jsx
@@ -127,63 +127,68 @@ const About = () => {
     );
   });
   return (
-    <div
-      ref={aboutContainerRef}
-      className="h-full sm:h-screen bg-dark-olive relative py-8 px-6 xl:text-2xl"
-    >
-      <div ref={wrapContainerRef}>
-        <div ref={aboutTitleRef}>
-          <h1 className="font-integral-extra-bold text-[40px] sm:text-7xl ml-8 sm:ml-14 ">
-            Who{" "}
-            <span className=" bg-black text-[#D7FF00] px-5 pb-4 pt-0 inline-block ">
-              AM I
-            </span>{" "}
-            ?
-          </h1>
-        </div>
-
-        <div
-          ref={cardRef}
-          className="flex flex-col md:flex-row w-full justify-center items-center"
-        >
-          <div className="w-full md:w-[40%] p-6 text-white text-[12px] sm:text-[16px] font-mono">
-            <p>
-              I'm Harishihan Chandrakumar — a Full Stack Developer, Designer,
-              and Artist.
-            </p>
-            <br />
-            <p>
-              {" "}
-              I love turning ideas into interactive experiences, whether it's
-              building web apps with the MERN stack, designing clean and
-              user-friendly interfaces, or creating artwork that inspires.
-            </p>{" "}
-            <br />
-            <p>
-              I enjoy solving real problems with code, experimenting with new
-              technologies, and bringing creativity into every project I work
-              on. I enjoy working across diverse domains — building web
-              applications, designing intuitive interfaces, and expressing ideas
-              through art
-            </p>
+    <>
+      <div
+        ref={aboutContainerRef}
+        className="min-h-screen bg-dark-olive relative py-8 px-6 xl:text-2xl"
+      >
+        <div ref={wrapContainerRef}>
+          <div ref={aboutTitleRef}>
+            <h1 className="font-integral-extra-bold text-[40px] sm:text-7xl ml-8 sm:ml-14 ">
+              Who{" "}
+              <span className=" bg-black text-[#D7FF00] px-5 pb-4 pt-0 inline-block ">
+                AM I
+              </span>{" "}
+              ?
+            </h1>
           </div>
-          <div className="w-full sm:w-[40%] flex flex-col justify-center items-center">
-            <img src={assets.me} alt="" className="w-80 -translate-y-4" />
-            <div className="w-full flex justify-center items-center">
-              <button className="w-48 py-2 bg-lime-primary">Download CV</button>
-              <button className="w-48 py-2 bg-black text-white ">
-                Reach Me
-              </button>
+
+          <div
+            ref={cardRef}
+            className="flex flex-col md:flex-row w-full justify-center items-center"
+          >
+            <div className="w-full md:w-[40%] p-6 text-white text-[12px] sm:text-[16px] font-mono">
+              <p>
+                I'm Harishihan Chandrakumar — a Full Stack Developer, Designer,
+                and Artist.
+              </p>
+              <br />
+              <p>
+                {" "}
+                I love turning ideas into interactive experiences, whether it's
+                building web apps with the MERN stack, designing clean and
+                user-friendly interfaces, or creating artwork that inspires.
+              </p>{" "}
+              <br />
+              <p>
+                I enjoy solving real problems with code, experimenting with new
+                technologies, and bringing creativity into every project I work
+                on. I enjoy working across diverse domains — building web
+                applications, designing intuitive interfaces, and expressing
+                ideas through art
+              </p>
+            </div>
+            <div className="w-full sm:w-[40%] flex flex-col justify-center items-center">
+              <img src={assets.me} alt="" className="w-80 -translate-y-4" />
+              <div className="w-full flex justify-center items-center">
+                <button className="w-48 py-2 bg-lime-primary">
+                  Download CV
+                </button>
+                <button className="w-48 py-2 bg-black text-white ">
+                  Reach Me
+                </button>
+              </div>
             </div>
           </div>
         </div>
-
-        {/* <p className="font-mono mx-auto w-3/4 text-center text-lime-primary mt-10 capitalize text-[14px]">
+      </div>
+      {/* <div className="h-[300px] flex justify-center items-center">
+        <p className="font-mono mx-auto w-3/4 text-center capitalize text-[14px]">
           [ development isn’t just about writing code — it’s about creating
           things that make an impact. ]
-        </p> */}
-      </div>
-    </div>
+        </p>
+      </div> */}
+    </>
   );
 };
 
