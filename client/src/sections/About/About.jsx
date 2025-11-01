308b8f55e9581418a2ca430fb3521e363b4ed076
harishihan-c <harishihan@outlook.com>
2025-11-01 17:12:29 +0530

    feat: desktop animation completed

--- client/src/sections/About/About.jsx
+++ client/src/sections/About/About.jsx
@@ -10,104 +10,173 @@ gsap.registerPlugin(ScrollTrigger);
 
 const About = () => {
   const aboutContainerRef = useRef(null);
-  const aboutTextRef = useRef(null);
+  const aboutTitleRef = useRef(null);
+  const cardRef = useRef(null);
+  const wrapContainerRef = useRef(null);
 
-  // useGSAP(() => {
-  //   const aboutTl = gsap.timeline({
-  //     scrollTrigger: {
-  //       trigger: aboutContainerRef.current,
-  //       start: "top 30%",
-  //       pin: true,
-  //       markers: true,
-  //     },
-  //   });
-  // });
+  useGSAP(() => {
+    gsap.set(aboutContainerRef.current, {
+      backgroundColor: " #ffffff",
+    });
 
+    //Pre Animation background color
+    let bgTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: aboutContainerRef.current,
+        start: "top 20%",
+        end: "bottom bottom",
+        scrub: true,
+        markers: true,
+      },
+    });
+
+    bgTl.to(aboutContainerRef.current, {
+      backgroundColor: "#282c20",
+      duration: 0.5,
+    });
+
+    let mm = gsap.matchMedia();
+
+    mm.add(
+      {
+        isMobile: "(max-width: 639px)",
+        isTab: "(max-width: 767px)",
+        isDesktop: "(max-width:1279px)",
+        isWide: "(min-width: 1280px)",
+      },
+      (context) => {
+        let { isMobile, isTab, isDesktop, isWide } = context.conditions;
+
+        // pre Animation Text and Card
+        if (isDesktop) {
+          let initTl = gsap.timeline({
+            scrollTrigger: {
+              trigger: aboutContainerRef.current,
+              scrub: true,
+              markers: true,
+              start: "top bottom",
+              end: "+=1000",
+            },
+          });
+
+          initTl.fromTo(
+            aboutTitleRef.current,
+            { x: 800, y: 150, autoAlpha: 0 },
+            {
+              y: 0,
+              x: 0,
+              scale: 0.6,
+              transformOrigin: "left top",
+              autoAlpha: 1,
+              duration: 1,
+              // ease: "power4.inOut",
+            }
+          );
+
+          initTl.fromTo(
+            cardRef.current,
+            { x: 1000, y: 150, autoAlpha: 0 },
+            {
+              x: 100,
+              y: 0,
+              autoAlpha: 1,
+              duration: 0.8,
+            },
+            "0.6"
+          );
+        }
+
+        //Actual Timeline
+        let tl = gsap.timeline({
+          scrollTrigger: {
+            trigger: aboutContainerRef.current,
+            start: "top top",
+            end: "+=500",
+            pin: true,
+            scrub: true,
+            markers: true,
+          },
+        });
+
+        tl.to(
+          aboutTitleRef.current,
+          {
+            scale: 0.45,
+            transformOrigin: "left top",
+            duration: 0.3,
+            color: "#ffffff",
+          },
+          "a"
+        );
+
+        tl.to(
+          cardRef.current,
+          {
+            x: 0,
+            duration: 0.3,
+          },
+          "a"
+        );
+      }
+    );
+  });
   return (
     <div
       ref={aboutContainerRef}
-      className="h-screen bg-dark-olive relative pt-5 px-6"
+      className="h-screen bg-dark-olive relative py-8 px-6 xl:text-2xl"
     >
-      <div>
-        <h1
-          ref={aboutTextRef}
-          className="font-integral-extra-bold text-5xl sm:text-7xl "
+      <div ref={wrapContainerRef}>
+        <div ref={aboutTitleRef}>
+          <h1 className="font-integral-extra-bold text-5xl sm:text-7xl ml-8 sm:ml-14 ">
+            Who{" "}
+            <span className=" bg-black text-[#D7FF00] px-5 pb-4 pt-0 inline-block ">
+              AM I
+            </span>{" "}
+            ?
+          </h1>
+        </div>
+
+        <div
+          ref={cardRef}
+          className="flex flex-col md:flex-row w-full justify-center items-center"
         >
-          Who{" "}
-          <span className=" bg-black text-[#D7FF00] px-5 pb-4 pt-0 inline-block ">
-            AM I
-          </span>{" "}
-          ?
-        </h1>
-      </div>
-
-      <div className="flex flex-col sm:flex-row w-full justify-center items-center">
-        <div className="w-full sm:w-[40%] p-6 text-white text-[15px] font-mono">
-          <p>
-            I'm Harishihan Chandrakumar — a Full Stack Developer, Designer, and
-            Artist.
-          </p>
-          <br />
-          <p>
-            {" "}
-            I love turning ideas into interactive experiences, whether it's
-            building web apps with the MERN stack, designing clean and
-            user-friendly interfaces, or creating artwork that inspires.
-          </p>{" "}
-          <br />
-          <p>
-            I enjoy solving real problems with code, experimenting with new
-            technologies, and bringing creativity into every project I work on.
-            I enjoy working across diverse domains — building web applications,
-            designing intuitive interfaces, and expressing ideas through art
-          </p>
-        </div>
-        <div className="w-full sm:w-[40%] flex flex-col justify-center items-center">
-          <img src={assets.me} alt="" className="w-80 -translate-y-4" />
-          <div className="w-full flex justify-center items-center">
-            <button className="w-48 py-2 bg-lime-primary">Download CV</button>
-            <button className="w-48 py-2 bg-black text-white ">
-              Reach Me
-            </button>
+          <div className="w-full md:w-[40%] p-6 text-white text-[16px] font-mono">
+            <p>
+              I'm Harishihan Chandrakumar — a Full Stack Developer, Designer,
+              and Artist.
+            </p>
+            <br />
+            <p>
+              {" "}
+              I love turning ideas into interactive experiences, whether it's
+              building web apps with the MERN stack, designing clean and
+              user-friendly interfaces, or creating artwork that inspires.
+            </p>{" "}
+            <br />
+            <p>
+              I enjoy solving real problems with code, experimenting with new
+              technologies, and bringing creativity into every project I work
+              on. I enjoy working across diverse domains — building web
+              applications, designing intuitive interfaces, and expressing ideas
+              through art
+            </p>
+          </div>
+          <div className="w-full sm:w-[40%] flex flex-col justify-center items-center">
+            <img src={assets.me} alt="" className="w-80 -translate-y-4" />
+            <div className="w-full flex justify-center items-center">
+              <button className="w-48 py-2 bg-lime-primary">Download CV</button>
+              <button className="w-48 py-2 bg-black text-white ">
+                Reach Me
+              </button>
+            </div>
           </div>
         </div>
+
+        {/* <p className="font-mono mx-auto w-3/4 text-center text-lime-primary mt-10 capitalize text-[14px]">
+          [ development isn’t just about writing code — it’s about creating
+          things that make an impact. ]
+        </p> */}
       </div>
-
-      <p className='font-mono mx-auto w-1/2 text-center text-lime-primary mt-10 capitalize'>
-        [ development isn’t just about writing code — it’s about creating things
-        that make an impact. ]
-      </p>
-
-      {/* <div className="w-[90%] flex items-center justify-evenly mx-auto px-4 py-4 ">
-        <p className="w-[90%] bg-[#D7FF00] px-8 py-11">
-          Hi, I’m Harishihan Chandrakumar — a Full Stack Developer, Designer,
-          and Artist.
-          <br /> <br />I love turning ideas into interactive experiences,
-          whether it’s building web apps with the MERN stack, designing clean
-          and user-friendly interfaces, or creating artwork that inspires.
-          <br /> <br /> I enjoy solving real problems with code, experimenting
-          with new technologies, and bringing creativity into every project I
-          work on. I enjoy working across diverse domains — building web
-          applications, designing intuitive interfaces, and expressing ideas
-          through art <br /> <br /> For me, development isn’t just about writing
-          code — it’s about creating things that make an impact.
-        </p>
-        {/* <div className="h-full flex flex-col items-center justify-center  ">
-          <div className="overflow-hidden">
-            <img
-            src={assets.me}
-            alt=""
-            className="w-sm objcet-cover relative bottom-8"
-          />
-          </div>
-          <div className="w-full ">
-            <button className="w-[50%] py-3 border">Download CV</button>
-            <button className="w-[50%] py-3 bg-black text-white">
-              Reach Me
-            </button>
-          </div>
-        </div> */}
-      {/* </div> */}
     </div>
   );
 };
