9d110373af3abb24fba45771fd0739a5e0f70c93
harishihan-c <harishihan@outlook.com>
2025-11-02 13:09:03 +0530

    feat: create working mini nav bar layout

--- client/src/components/StackNav.jsx
+++ client/src/components/StackNav.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useState } from "react";
+import All from "./All";
+import Frontend from "./Frontend";
+import Backend from "./Backend";
+import Languages from "./Languages";
+import Tools from "./Tools";
 
 const StackNav = () => {
+  const [active, setActive] = useState("all");
+
   return (
     <div>
+      <nav>
+        <button onClick={() => setActive("all")}>All</button>
+        <button onClick={() => setActive("frontend")}>Frontend</button>
+        <button onClick={() => setActive("backend")}>Backend</button>
+        <button onClick={() => setActive("languages")}>Languages</button>
+        <button onClick={() => setActive("tools")}>Tools</button>
+      </nav>
       
+      <div>
+        {active === "all" && <All />}
+        {active === "frontend" && <Frontend />}
+        {active === "backend" && <Backend />}
+        {active === "languages" && <Languages />}
+        {active === "tools" && <Tools />}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default StackNav
+export default StackNav;
