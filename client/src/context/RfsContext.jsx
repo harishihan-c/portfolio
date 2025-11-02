0863c7b964b8aadce06439e2db2e0dab40d1f622
harishihan-c <harishihan@outlook.com>
2025-11-02 21:41:10 +0530

    feat: Create context file and add all userefs of All page

---
+++ client/src/context/RfsContext.jsx
@@ -0,0 +1,55 @@
+import { createContext, useContext, useRef } from "react";
+
+export const RfsContext = createContext();
+
+export const RfsProvider = () => {
+  const mainContainer = useRef(null);
+  const cssRef = useRef(null);
+  const expressRef = useRef(null);
+  const figmaRef = useRef(null);
+  const fireRef = useRef(null);
+  const gitRef = useRef(null);
+  const githubRef = useRef(null);
+  const htmlRef = useRef(null);
+  const ideaRef = useRef(null);
+  const javaRef = useRef(null);
+  const jsRef = useRef(null);
+  const mongoRef = useRef(null);
+  const sqlRef = useRef(null);
+  const nodeRef = useRef(null);
+  const postmanRef = useRef(null);
+  const tailwindRef = useRef(null);
+  const viteRef = useRef(null);
+  const vscodeRef = useRef(null);
+  const webstromRef = useRef(null);
+
+  return (
+    <RfsContext.Provider
+      value={{
+        mainContainer,
+        cssRef,
+        expressRef,
+        figmaRef,
+        fireRef,
+        gitRef,
+        githubRef,
+        htmlRef,
+        ideaRef,
+        javaRef,
+        jsRef,
+        mongoRef,
+        sqlRef,
+        nodeRef,
+        postmanRef,
+        tailwindRef,
+        viteRef,
+        vscodeRef,
+        webstromRef,
+      }}
+    >
+      {children}
+    </RfsContext.Provider>
+  );
+};
+
+export const useRefs = () => useContext(RfsContext);
