ad45e6c3d10709f3d31baf5cd21b4ef147895084
harishihan-c <harishihan@outlook.com>
2025-11-02 15:14:13 +0530

    feat: Add title responsive text

--- client/src/components/All.jsx
+++ client/src/components/All.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const All = () => {
   return (
-    <div>All</div>
-  )
-}
+    <div>
+      {" "}
+      <div className="">
+        <div className="font-integral-extra-bold text-[40px] sm:text-7xl  w-[70%] mx-auto text-center">
+          The{" "}
+          <span className=" bg-black text-[#D7FF00] px-5 pb-4 pt-0 inline-block ">
+            Stack
+          </span>{" "}
+          <p>Behind My Work</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default All
+export default All;
