d1b4747877eb776035fa7f1a8743d921791863b4
harishihan-c <harishihan@outlook.com>
2025-11-02 17:57:17 +0530

    feat: change color when click the button

--- client/src/components/StackNav.jsx
+++ client/src/components/StackNav.jsx
@@ -8,16 +8,57 @@ import Tools from "./Tools";
 const StackNav = () => {
   const [active, setActive] = useState("all");
 
+  const nameArray = ["all", "frontend", "backend", "languages", "tools"];
+
+  const handleOnClick = (index) => {
+    setActive(() => nameArray[index]);
+  };
+
   return (
-    <div>
-      <nav>
-        <button onClick={() => setActive("all")}>All</button>
-        <button onClick={() => setActive("frontend")}>Frontend</button>
-        <button onClick={() => setActive("backend")}>Backend</button>
-        <button onClick={() => setActive("languages")}>Languages</button>
-        <button onClick={() => setActive("tools")}>Tools</button>
+    <div className="h-screen flex flex-col">
+      <nav className="flex items-center justify-center gap-8 h-[30%] ">
+        <button
+          onClick={() => handleOnClick(0)}
+          className={`${
+            active === nameArray[0] ? "text-lime-primary bg-black" : ""
+          } px-6  py-1.5`}
+        >
+          All
+        </button>
+        <button
+          onClick={() => handleOnClick(1)}
+          className={`${
+            active === nameArray[1] ? "text-lime-primary bg-black" : ""
+          } px-6 py-1.5`}
+        >
+          Frontend
+        </button>
+        <button
+          onClick={() => handleOnClick(2)}
+          className={`${
+            active === nameArray[2] ? "text-lime-primary bg-black" : ""
+          }  px-6 py-1.5`}
+        >
+          Backend
+        </button>
+        <button
+          onClick={() => handleOnClick(3)}
+          className={`${
+            active === nameArray[3] ? "text-lime-primary bg-black" : ""
+          }  px-6 py-1.5`}
+        >
+          Languages
+        </button>
+        <button
+          onClick={() => handleOnClick(4)}
+          className={`${
+            active === nameArray[4] ? "text-lime-primary bg-black" : ""
+          }  px-6 py-1.5`}
+        >
+          Tools
+        </button>
       </nav>
-      
+
       <div>
         {active === "all" && <All />}
         {active === "frontend" && <Frontend />}
