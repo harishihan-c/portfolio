ff6259d493226b23dc4e0d20d571c06e80cac01f
harishihan-c <harishihan@outlook.com>
2025-11-02 17:30:55 +0530

    chore: (assets) Add skill icons

--- client/src/assets/images/assets.js
+++ client/src/assets/images/assets.js
@@ -1,5 +1,41 @@
 import me from "./Group 21.png";
+import css from "./CSS.svg";
+import express from "./ExpressJS-Dark.svg";
+import figma from "./Figma-Dark.svg";
+import firebase from "./Firebase-Dark.svg";
+import git from "./Git.svg";
+import github from "./Github-Dark.svg";
+import html from "./HTML.svg";
+import idea from "./Idea-Dark.svg";
+import java from "./Java-Dark.svg";
+import js from "./JavaScript.svg";
+import mongodb from "./MongoDB.svg";
+import mysql from "./MySQL-Dark.svg";
+import node from "./NodeJS-Dark.svg";
+import postman from "./Postman.svg";
+import tailwind from "./TailwindCSS-Dark.svg";
+import vite from "./Vite-Dark.svg";
+import vscode from "./VSCode-Dark.svg";
+import webstrom from "./WebStorm-Dark.svg";
 
 export const assets = {
   me,
+  css,
+  figma,
+  firebase,
+  git,
+  github,
+  html,
+  java,
+  idea,
+  js,
+  mongodb,
+  postman,
+  tailwind,
+  vite,
+  vscode,
+  webstrom,
+  express,
+  mysql,
+  node,
 };
