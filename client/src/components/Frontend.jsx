dc1f27c01fc1c71e8178b9d198e61b94bf4d4a09
harishihan-c <harishihan@outlook.com>
2025-11-04 01:44:30 +0530

    feat: add responsiveness to the component

--- client/src/components/Frontend.jsx
+++ client/src/components/Frontend.jsx
@@ -11,16 +11,16 @@ const Frontend = () => {
   ];
 
   return (
-    <div className="w-[70%] grid grid-cols-1 md:grid-cols-2  gap-y-8 gap-10 mx-auto">
+    <div className="w-[70%]  grid grid-cols-1 lg:grid-cols-2  gap-y-8  gap-10 mx-auto ">
       {frontendArray.map((id) => (
-        <div className=" h-16 relative flex ">
+        <div className="h-14 sm:h-16 relative flex ">
           <div className="w-4 bg-lime-primary"></div>
-          <img src={assets[id]} alt="" className="ml-3 w-13 mr-8" />
+          <img src={assets[id]} alt="" className="ml-3 w-11 sm:w-13 mr-8" />
           <div
-            className="w-full absolute h-full bg-lime-primary
+            className="w-80 absolute left-7 h-full bg-lime-primary
          [clip-path:inset(25%_55%_20%_23%)]"
           ></div>
-          <span className=" font-integral-demi-bold my-auto text-2xl z-10">
+          <span className=" font-integral-demi-bold my-auto text-xl sm:text-2xl z-10">
             {id}
           </span>
         </div>
