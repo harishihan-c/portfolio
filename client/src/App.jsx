4ace774105c620f0ea193f6457a3764dc3eb615e
harishihan-c <harishihan@outlook.com>
2025-11-04 23:38:26 +0530

    refactor: cleanup the code put all animation related code into app component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import NavBar from "./components/NavBar";
 import Home from "./sections/Home";
 import About from "./sections/About";
@@ -8,10 +8,69 @@ import Contact from "./sections/Contact";
 
 import Lenis from "lenis";
 import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
+import { useRefs } from "./context/RfsContext";
+
+gsap.registerPlugin(useGSAP);
 gsap.registerPlugin(ScrollTrigger);
 const App = () => {
+  const {
+    cssRef,
+    expressRef,
+    figmaRef,
+    fireRef,
+    gitRef,
+    githubRef,
+    htmlRef,
+    ideaRef,
+    javaRef,
+    jsRef,
+    mongoRef,
+    sqlRef,
+    nodeRef,
+    postmanRef,
+    tailwindRef,
+    viteRef,
+    vscodeRef,
+    webstromRef,
+    titleRef,
+    reactRef,
+    navRef,
+    skillsContainerRef,
+    wrapperRef,
+  } = useRefs();
+
+  //aRAY
+  const iconArrayLeft = [
+    webstromRef,
+    figmaRef,
+    nodeRef,
+    postmanRef,
+    tailwindRef,
+    reactRef,
+  ];
+
+  const iconArrayRight = [
+    githubRef,
+    expressRef,
+    gitRef,
+    cssRef,
+    sqlRef,
+    viteRef,
+  ];
+
+  const iconArrayBottom = [
+    fireRef,
+    ideaRef,
+    vscodeRef,
+    jsRef,
+    htmlRef,
+    javaRef,
+    mongoRef,
+  ];
+
   useEffect(() => {
     // Initialize a new Lenis instance for smooth scrolling
     const lenis = new Lenis();
@@ -29,6 +88,98 @@ const App = () => {
     gsap.ticker.lagSmoothing(0);
   }, []);
 
+  useGSAP(() => {
+    //Initial set values
+    gsap.set(navRef.current, { autoAlpha: 0 });
+    gsap.set(titleRef.current, { autoAlpha: 0, z: -1000 });
+
+    let mm = gsap.matchMedia();
+
+    mm.add(
+      {
+        isMobile: "(max-width: 639px)",
+        isDesktop: "(min-width: 640px)",
+      },
+      (context) => {
+        let { isMobile, isDesktop } = context.conditions;
+
+        //  pre Animation
+        let preTl = gsap.timeline({
+          scrollTrigger: {
+            trigger: wrapperRef.current,
+            start: "top 90%",
+            end: "bottom bottom",
+            scrub: true,
+            markers: true,
+            anticipatePin: 1.5,
+          },
+        });
+
+        preTl.to(
+          titleRef.current,
+          {
+            autoAlpha: 1,
+            z: 0,
+            duration: 1,
+            ease: "power1.inOut",
+          },
+          "<0.2"
+        );
+
+        let masterTl = gsap.timeline({
+          scrollTrigger: {
+            trigger: wrapperRef.current,
+            start: "top top",
+            end: "+=200%",
+            pin: true,
+            scrub: true,
+          },
+        });
+
+
+        iconArrayRight.forEach((ref) => {
+          masterTl.fromTo(
+            ref.current,
+            {
+              x: 200,
+              y: 400,
+              rotate: 40,
+            },
+            {
+              x: 0,
+              y: 0,
+              rotate: 0,
+              ease: "power3.inOut",
+              duration: isMobile ? 0.4 : 0.7,
+            },
+            "a"
+          );
+        });
+
+        iconArrayLeft.forEach((ref) => {
+          masterTl.fromTo(
+            ref.current,
+            {
+              x: -200,
+              y: 400,
+              rotate: -90,
+              autoAlpha: 0,
+            },
+            {
+              autoAlpha: 1,
+              x: 0,
+              y: 0,
+              rotate: 80,
+              ease: "power3.inOut",
+              duration: isMobile ? 0.4 : 0.7,
+            },
+            "a"
+          );
+        });
+      }
+    );
+  });
+
   return (
     <div>
       <NavBar />
@@ -38,12 +189,14 @@ const App = () => {
       <section id="about">
         <About />
       </section>
-      <section id="skills">
-        <Skills />
-      </section>
-      <section id="projects">
-        <Projects />
-      </section>
+      <div ref={wrapperRef} id="wrapper">
+        <section id="skills">
+          <Skills />
+        </section>
+        <section id="projects">
+          <Projects />
+        </section>
+      </div>
       <section id="contact">
         <Contact />
       </section>
--- client/src/components/StackNav.jsx
+++ client/src/components/StackNav.jsx
@@ -4,240 +4,60 @@ import Frontend from "./Frontend";
 import Backend from "./Backend";
 import Languages from "./Languages";
 import Tools from "./Tools";
-import { gsap } from "gsap";
-import { useGSAP } from "@gsap/react";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useRefs } from "../context/RfsContext";
 
-gsap.registerPlugin(useGSAP);
-gsap.registerPlugin(ScrollTrigger);
 
 const StackNav = () => {
   const {
-    mainContainer,
-    cssRef,
-    expressRef,
-    figmaRef,
-    fireRef,
-    gitRef,
-    githubRef,
-    htmlRef,
-    ideaRef,
-    javaRef,
-    jsRef,
-    mongoRef,
-    sqlRef,
-    nodeRef,
-    postmanRef,
-    tailwindRef,
-    viteRef,
-    vscodeRef,
-    webstromRef,
-    titleRef,
-    reactRef,
+    skillsContainerRef,
+    navRef,
   } = useRefs();
 
-  //aRAY
-  const iconArrayLeft = [
-    cssRef,
-    expressRef,
-    figmaRef,
-    fireRef,
-    tailwindRef,
-    githubRef,
-    ideaRef,
-    reactRef,
-  ];
 
-  const iconArrayRight = [
-    gitRef,
-    htmlRef,
-    javaRef,
-    jsRef,
-    mongoRef,
-    sqlRef,
-    nodeRef,
-    postmanRef,
-    viteRef,
-    vscodeRef,
-    webstromRef,
-  ];
+  const [active, setActive] = useState("All");
 
-  const [active, setActive] = useState("all");
-
-  const nameArray = ["all", "frontend", "backend", "languages", "tools"];
+  const nameArray = ["All", "Frontend", "Backend", "Languages", "Tools"];
 
   const handleOnClick = (index) => {
     setActive(() => nameArray[index]);
   };
-
-  //Main Component useRefs,
-  const skillsContainer = useRef(null);
-  const navRef = useRef(null);
-
-  // <<<<<<<-------------Animations---------------------------->>>>>>>>>>>>
-  useGSAP(() => {
-    //Initial set values
-    gsap.set(navRef.current, { autoAlpha: 0 });
-    gsap.set(titleRef.current, { autoAlpha: 0, z: -1000 });
-
-    let mm = gsap.matchMedia();
-
-    mm.add(
-      {
-        isMobile: "(max-width: 639px)",
-        isDesktop: "(min-width: 640px)",
-      },
-      (context) => {
-        let { isMobile, isDesktop } = context.conditions;
-
-        //pre Animation
-        let preTl = gsap.timeline({
-          scrollTrigger: {
-            trigger: skillsContainer.current,
-            start: "top 90%",
-            end: "bottom bottom",
-            scrub: true,
-            markers: true,
-            anticipatePin: 1
-          },
-        });
-
-        preTl.to(
-          titleRef.current,
-          {
-            autoAlpha: 1,
-            z: 0,
-            duration: 10,
-            ease: "power1.inOut",
-          },
-          "<0.2"
-        );
-
-        let tl = gsap.timeline({
-          scrollTrigger: {
-            trigger: skillsContainer.current,
-            start: "top top",
-            end: isDesktop? "+=2000": "+=3000",
-            scrub: true,
-            pin: true,
-            pinSpacing: true,
-            markers: true,
-          },
-        });
-
-        tl.to(
-          navRef.current,
-          {
-            autoAlpha: 1,
-            duration: 0.7,
-          },
-          "a"
-        );
-
-        iconArrayLeft.forEach((ref) => {
-          tl.fromTo(
-            ref.current,
-            {
-              y: -25,
-              rotate: -5,
-              // duration: 0.9,
-            },
-            {
-              y: 10,
-              rotate: 0,
-              // repeat: -1,
-              // yoyo: true,
-              ease: "power3.inOut",
-              duration: isMobile?0.4 : 0.9,
-            },
-            "a"
-          );
-        });
-
-        iconArrayRight.forEach((ref) => {
-          tl.fromTo(
-            ref.current,
-            {
-              y: -25,
-              rotate: 10,
-              // duration: 0.9,
-            },
-            {
-              y: 10,
-              rotate: 0,
-              // repeat: -1,
-              yoyo: true,
-              ease: "power3.inOut",
-              duration: isMobile?0.4 : 0.9,
-            },
-            "a"
-          );
-        });
-      }
-    );
-  });
-
+  
   return (
     <div
-      ref={skillsContainer}
-      className="min-h-screen w-screen flex flex-col relative perspective-distant "
+      ref={skillsContainerRef}
+      className="h-screen w-screen flex flex-col relative perspective-distant bg-amber-300"
     >
       <nav
         ref={navRef}
         className="flex items-center justify-center gap-8 h-[30%] w-full"
       >
-        <button
-          onClick={() => handleOnClick(0)}
-          className={`${
-            active === nameArray[0] ? "text-lime-primary bg-black" : ""
-          } px-6  py-1.5`}
-        >
-          All
-        </button>
-        <button
-          onClick={() => handleOnClick(1)}
-          className={`${
-            active === nameArray[1] ? "text-lime-primary bg-black" : ""
-          } px-6 py-1.5`}
-        >
-          Frontend
-        </button>
-        <button
-          onClick={() => handleOnClick(2)}
-          className={`${
-            active === nameArray[2] ? "text-lime-primary bg-black" : ""
-          }  px-6 py-1.5`}
-        >
-          Backend
-        </button>
-        <button
-          onClick={() => handleOnClick(3)}
-          className={`${
-            active === nameArray[3] ? "text-lime-primary bg-black" : ""
-          }  px-6 py-1.5`}
-        >
-          Languages
-        </button>
-        <button
-          onClick={() => handleOnClick(4)}
-          className={`${
-            active === nameArray[4] ? "text-lime-primary bg-black" : ""
-          }  px-6 py-1.5`}
-        >
-          Tools
-        </button>
+        {nameArray.map((id, index) => (
+          <button
+            key={id}
+            onClick={() => handleOnClick(index)}
+            className={`${
+              active === id ? "text-lime-primary bg-black" : ""
+            } px-6  py-1.5`}
+          >
+            {id}
+          </button>
+        ))}
       </nav>
 
       <div>
-        {active === "all" && <All />}
-        {active === "frontend" && <Frontend />}
-        {active === "backend" && <Backend />}
-        {active === "languages" && <Languages />}
-        {active === "tools" && <Tools />}
+        {active === "All" && <All />}
+        {active === "Frontend" && <Frontend />}
+        {active === "Backend" && <Backend />}
+        {active === "Languages" && <Languages />}
+        {active === "Tools" && <Tools />}
       </div>
     </div>
   );
 };
 
 export default StackNav;
+
+
+      
+       
+        
