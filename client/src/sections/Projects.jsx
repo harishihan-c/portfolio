9665400f946ee6de0d946743559462c3e2302011
harishihan-c <harishihan@outlook.com>
2025-11-04 20:45:33 +0530

    feat: project page timeline created

--- client/src/sections/Projects.jsx
+++ client/src/sections/Projects.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React,  {useRef} from 'react'
+import {gsap} from 'gsap'
+import { useGSAP } from '@gsap/react'
+import ScrollTrigger from 'gsap/ScrollTrigger'
+
+gsap.registerPlugin(useGSAP);
+gsap.registerPlugin(ScrollTrigger)
 
 const Projects = () => {
+
+  const projectContainerRef = useRef(null)
+
+  useGSAP(() => {
+    let mm = gsap.matchMedia()
+
+    mm.add({
+      isMobile: "(max-width: 639px)",
+      isDesktop: "(min-width: 640px)"
+    }, context => {
+      let {isMobile, isDesktop} = context.conditions
+
+      let tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: projectContainerRef.current,
+          start: "top bottom",
+          end: "+=2000",
+          pin: true,
+          scrub: true
+        }
+      })
+    })
+  })
   return (
-    <div className='h-screen'>Projects</div>
+    <div ref={projectContainerRef} className='h-screen bg-amber-400'>Projects</div>
   )
 }
 
