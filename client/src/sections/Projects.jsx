e9a5c94deaf118ead2b084658479b80d9e3f6843
harishihan-c <harishihan@outlook.com>
2025-11-06 23:21:15 +0530

    refactor: cleanup the code

--- client/src/sections/Projects.jsx
+++ client/src/sections/Projects.jsx
@@ -17,18 +17,6 @@ const Projects = () => {
 
   const mainContainer = useRef(null);
 
-  // useGSAP(() => {
-  //   const tl =gsap.timeline({
-  //     scrollTrigger: {
-  //       trigger: mainContainer.current,
-  //       start: "top top",
-  //       end: "+=100%",
-  //       pin: mainContainer.current,
-
-  //     }
-  //   })
-  // })
-
   return (
     <div ref={mainContainer} className="min-h-screen bg-[#E6E6E6] px-10 sm:px-20">
       <div className="flex justify-between items-center py-8 font-integral-regular text-[20px] ">
@@ -41,14 +29,14 @@ const Projects = () => {
           </div>
         ))}
       </div>
-      {/* <div className="flex items-center justify-center mt-30">
+      <div className="flex items-center justify-center mt-30">
         <div className="font-integral-extra-bold text-[40px] sm:text-5xl lg:text-7xl w-[60%]  sm:w-[70%]  mx-auto text-center mb-10">
           <p>Things I've</p>{" "}
           <span className=" bg-black text-[#D7FF00] px-5 pb-4 pt-0 inline-block ">
             Built
           </span>{" "}
         </div>
-      </div> */}
+      </div>
       <div>
         {active === navArray[0] && <ShowProjects />}
         {active === navArray[1] && <ShowDesigns />}
