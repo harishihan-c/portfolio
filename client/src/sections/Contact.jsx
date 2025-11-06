5cebeffc08d2ec1003b13a6eb79ed684f4c85c89
harishihan-c <harishihan@outlook.com>
2025-11-06 14:44:57 +0530

    feat: add title page

--- client/src/sections/Contact.jsx
+++ client/src/sections/Contact.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div className='h-screen'>Contact</div>
+    <div className='h-screen'>
+      <div className="flex items-center justify-center mt-30">
+        <div className="font-integral-extra-bold text-[40px] sm:text-5xl lg:text-7xl w-[60%]  sm:w-[70%]  mx-auto text-center mb-10">
+          <p>Let's Built something</p>{" "}
+          <span className=" bg-black text-[#D7FF00] px-5 pb-4 pt-0 inline-block ">
+            Built
+          </span>{" "}
+        </div>
+      </div>
+    </div>
   )
 }
 
