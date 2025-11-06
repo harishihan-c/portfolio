8497bd1073d7aa21aa901b78c3549d80b4896164
harishihan-c <harishihan@outlook.com>
2025-11-06 14:19:06 +0530

    feat: add show design card layout

--- client/src/components/ShowDesigns.jsx
+++ client/src/components/ShowDesigns.jsx
@@ -1,9 +1,114 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const ShowDesigns = () => {
-  return (
-    <div>ShowDesigns</div>
-  )
-}
+  const projectData = [
+    {
+      id: 1,
+      title: "Skill Forge",
+      img: "",
+      text: "A web platform where learners can generate AI-powered roadmaps to discover and learn new skills.",
+      background: "bg-black",
+      button: "white",
+      butonText: "black",
+    },
+    {
+      id: 2,
+      title: "Note Taking app",
+      img: "",
+      text: "A simple yet powerful app to create, organize, and manage notes with ease",
+      background: "bg-white",
+      button: "lime-primary",
+      buttonText: "black",
+    },
+    {
+      id: 3,
+      title: "Gemini Clone",
+      img: "",
+      text: "An AI-powered web app to ask questions and generate content using the Gemini API.",
+      background: "bg-lime-primary",
+      button: "black",
+      buttonText: "white",
+    },
+    {
+      id: 4,
+      title: "Skill Forge",
+      img: "",
+      text: "A web platform where learners can generate AI-powered roadmaps to discover and learn new skills.",
+      background: "bg-black",
+      button: "white",
+      butonText: "black",
+    },
+    {
+      id: 5,
+      title: "Note Taking app",
+      img: "",
+      text: "A simple yet powerful app to create, organize, and manage notes with ease",
+      background: "bg-white",
+      button: "lime-primary",
+      buttonText: "black",
+    },
+    {
+      id: 6,
+      title: "Gemini Clone",
+      img: "",
+      text: "An AI-powered web app to ask questions and generate content using the Gemini API.",
+      background: "bg-lime-primary",
+      button: "black",
+      buttonText: "white",
+    },
+  ];
 
-export default ShowDesigns
+  const [visible, setVisible] = useState(3);
+
+  return (
+    <div>
+      <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mt-10 gap-y-9 ">
+        {projectData.slice(0, visible).map((item) => (
+          <div key={item.id} className={`min-h-96 ${item.background} `}>
+            <h1
+              className={`text-center font-integral-medium text-2xl pt-4 ${
+                item.background === "bg-black" ? "text-white" : ""
+              }`}
+            >
+              {item.title}
+            </h1>
+            <div className="bg-amber-300 h-[50%] mt-2 mx-6 mb-3">
+              <img src={item.img} alt="" />
+            </div>
+            <div>{item.icons}</div>
+            <p
+              className={`h-[15%] text-[10px] sm:text-[13px] text-center  mx-6  ${
+                item.background === "bg-black" ? "text-white" : ""
+              }`}
+            >
+              {item.text}
+            </p>
+            <div className="flex items-center justify-center mt-4 max-w-full mx-6 gap-3">
+              <button
+                className={`w-1/2 py-2 bg-${item.button} text-${item.buttonText} text-[12px] sm:text-[14px]`}
+              >
+                Figma
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="w-full flex justify-center ">
+        <button
+          className="w-30 h-10 bg-white my-10 "
+          onClick={() => {
+            if (visible <= 3) {
+              setVisible(visible + 3);
+            } else {
+              setVisible(visible - 3);
+            }
+          }}
+        >
+          {visible <= 3 ? "Show More" : "Show Less"}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ShowDesigns;
