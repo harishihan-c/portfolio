a37c2dd9cc7b2d4bcdf861bbd2766a05cd8bce4a
harishihan-c <harishihan@outlook.com>
2025-11-09 15:50:31 +0530

    add image again p1

---
+++ client/src/assets/images/assets.js
@@ -0,0 +1,81 @@
+import me from "./Me.png";
+import css from "./CSS.svg";
+import express from "./ExpressJS-Dark.svg";
+import figma from "./Figma-Dark.svg";
+import firebase from "./Firebase-Dark.svg";
+import git from "./Git.svg";
+import github from "./Github-Dark.svg";
+import html5 from "./HTML.svg";
+import idea from "./Idea-Dark.svg";
+import java from "./Java-Dark.svg";
+import javascript from "./JavaScript.svg";
+import mongodb from "./MongoDB.svg";
+import mysql from "./MySQL-Dark.svg";
+import node from "./NodeJS-Dark.svg";
+import postman from "./Postman.svg";
+import tailwindcss from "./TailwindCSS-Dark.svg";
+import vite from "./Vite-Dark.svg";
+import vscode from "./VSCode-Dark.svg";
+import webstrom from "./WebStorm-Dark.svg";
+import react from "./React-Dark.svg";
+import instagram from "./instagram.png";
+import facebook from "./facebook.png";
+import linkedin from "./linked-in.png";
+import githubicon from "./Github.png";
+import girlFace from "./drawings/girl-face.jpg";
+import girlSide from "./drawings/girl-side.jpg";
+import girl from "./drawings/girl.jpg";
+import hamilton from "./drawings/hamilton.jpg";
+import witcher from "./drawings/witcher.jpg";
+
+import auth from "./projects/auth.png";
+import cloth from "./projects/cloth.png";
+import gemoni from "./projects/gemini.png";
+import movie from "./projects/movie.png";
+import note from "./projects/Note-App.png";
+import skillForge from "./projects/skill-forge.png";
+
+import beatroot from "./designs/beatroot.jpg";
+import chatapp from "./designs/chatapp.png";
+import forestapp from "./designs/forestapp.png";
+
+export const assets = {
+  me,
+  "css 3": css,
+  figma: figma,
+  firebase: firebase,
+  git: git,
+  "git hub": github,
+  "html 5": html5,
+  java: java,
+  "intellij idea": idea,
+  javascript: javascript,
+  "mongo db": mongodb,
+  postman: postman,
+  "tailwind css": tailwindcss,
+  vite: vite,
+  "vs code": vscode,
+  "web strom": webstrom,
+  "express js": express,
+  mysql: mysql,
+  "node js": node,
+  react: react,
+  instagram,
+  linkedin,
+  facebook,
+  githubicon,
+  girlFace,
+  girlSide,
+  girl,
+  hamilton,
+  witcher,
+  auth,
+  cloth,
+  gemoni,
+  movie,
+  note,
+  skillForge,
+  chatapp,
+  forestapp,
+  beatroot,
+};
